07df2912296bc9f64a3e627d694b27480aa7f0cb
Vi3301 <cryptornix@gmail.com>
2023-06-23 14:33:10 +0300

    add func main()

--- Weather.go
+++ Weather.go
@@ -1,27 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
-type Сity struct {
+type City struct {
 	lat  float64
 	long float64
 }
 
-func WeatherApiRequest(c Сity) (string, error) {
-	url := fmt.Sprintf(`https://api.openweathermap.org/data/3.0/onecall?lat=%v&lon=%v&appid={"API_Key"}`, c.lat, c.long)
+func WeatherApiRequest(c City) (string, error) {
+	apikey := os.Getenv("WEATHER_APITOKEN")
+	url := fmt.Sprintf(`https://api.openweathermap.org/data/3.0/onecall?lat=%v&lon=%v&appid=%v`, c.lat, c.long, apikey)
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return " ", err
+	}
 	resp, err := client.Do(req)
-	// узнать что за Body
+	if err != nil {
+		return " ", err
+	}
 	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return " ", err
+	}
 	defer resp.Body.Close()
+	var data string
 	json.Unmarshal(body, &data)
-
+	fmt.Println(data)
+	return data, nil
 }
 func main() {
-
+	var olhovka City = City{lat: 33.44, long: -94.04}
+	res, err := WeatherApiRequest(olhovka)
+	fmt.Println(res)
+	fmt.Println(err)
 }
